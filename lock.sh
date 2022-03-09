0fc0ffb901adab18fd1029c7ab9a8c3665ccdc77
Ethan M. Lee <ethan@mlee.xyz>
2022-03-08 22:32:39 -0600

    only restart networking if networking is not up

--- lock.sh
+++ lock.sh
@@ -8,5 +8,5 @@ MSG=$(fortune -s | cowsay -f turtle) && physlock -mp $MSG
 # after lock
 echo $(date '+%a %d.%m.%+4Y %I:%M %p %Z') > /tmp/curTime.tmp
 refreshbar.sh
-doas nmcli networking off && sleep 1 && doas nmcli networking on
+[ $(cat /sys/class/net/w*/operstate) != "up" ] && doas nmcli networking off && sleep 1 && doas nmcli networking on
 autorandr --change
