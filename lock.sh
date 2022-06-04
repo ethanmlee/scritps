0e5436daad0369566539c3728745138a74556bda
Ethan M. Lee <ethan@mlee.xyz>
2022-06-04 16:57:14 -0400

    remove doas from restarting network

--- lock.sh
+++ lock.sh
@@ -9,5 +9,5 @@ MSG=$(fortune -s | cowsay -f turtle) && physlock -mp $MSG
 # after lock
 echo $(date '+%a %d.%m.%+4Y %I:%M %p %Z') > /tmp/curTime.tmp
 refreshbar.sh
-[ $(cat /sys/class/net/w*/operstate) != "up" ] && doas nmcli networking off && sleep 1 && doas nmcli networking on
+[ $(cat /sys/class/net/w*/operstate) != "up" ] && nmcli networking off && sleep 1 && nmcli networking on
 autorandr --change
