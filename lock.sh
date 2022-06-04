7c3f842dcd17d82048f1cd0660d070c20d285c0d
Ethan M. Lee <ethan@mlee.xyz>
2022-06-04 16:54:51 -0400

    lock keepassxc bc I am overiding default actions on lid close

--- lock.sh
+++ lock.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 # before lock
+dbus-send --print-reply --dest=org.keepassxc.KeePassXC.MainWindow /keepassxc org.keepassxc.MainWindow.lockAllDatabases
 
 #lock script
 MSG=$(fortune -s | cowsay -f turtle) && physlock -mp $MSG
