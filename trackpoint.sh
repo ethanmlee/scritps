2f79d8dc6db40901d3d48aec80a982084e6a0687
Ethan M. Lee <ethan@mlee.xyz>
2022-12-10 18:02:52 -0500

    trackpoint.sh 2

--- trackpoint.sh
+++ trackpoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat /tmp/trackpoint.tmp; [ $? = 1 ] && touch /tmp/trackpoint.tmp && xinput set-prop "Logitech USB-PS/2 Optical Mouse" "Device Enabled" 0 && echo "trackpoint off" && exit
-cat /tmp/tackpoint.tmp; [ $? = 0 ] && rm /tmp/trackpoint.tmp && xinput set-prop "Logitech USB-PS/2 Optical Mouse" "Device Enabled" 1 && echo "trackpoint on" && exit
+cat /tmp/trackpoint.tmp; [ $? = 1 ] && touch /tmp/trackpoint.tmp && xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" 0 && echo "trackpoint off" && exit
+cat /tmp/tackpoint.tmp; [ $? = 0 ] && rm /tmp/trackpoint.tmp && xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" 1 && echo "trackpoint on" && exit
 
