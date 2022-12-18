abc41db6898eecfbfb3c1d5a3fbdf97031783e5b
Ethan M. Lee <ethan@mlee.xyz>
2022-12-18 03:48:31 -0500

    remove autorandr

--- event_handler.sh
+++ event_handler.sh
@@ -23,14 +23,14 @@ button/lid)
   close)
     # if not in desktop mode and currently in default autorandr profile
     cat /tmp/desktop_mode.tmp
-    if [ $? = 1 ] && [ $(autorandr --detected) = "default" ]; then
+    if [ $? = 1 ]; then
       # then wait 5 seconds if lid is closed
       grep -q closed /proc/acpi/button/lid/*/state
-      if [ $? = 0 ] && [ $(autorandr --current) = "default" ]; then
+      if [ $? = 0 ]; then
         sleep 5
         # if lid is still closed after 5 seconds then run through lock/sleep if statements
         grep -q closed /proc/acpi/button/lid/*/state
-        if [ $? = 0 ] && [ $(autorandr --current) = "default" ]; then
+        if [ $? = 0 ]; then
           # if not locked then lock and suspend else just suspend without locking
 	  UPOWER=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state" | awk '{ print $NF }')
 	  if [ "$UPOWER" = "charging" ] || [ "$UPOWER" = "fully-charged" ]; then
@@ -38,7 +38,7 @@ button/lid)
             [ $? != 0 ] && ~/.scripts/lock.sh
           else
             pgrep -f physlock > /dev/null
-            [ $? != 0 ] && ~/.scripts/lock.sh & doas pm-suspend || doas pm-suspend
+            [ $? != 0 ] && ~/.scripts/lock.sh & doas systemctl suspend || doas systemctl suspend
 	  fi
         fi
       fi
