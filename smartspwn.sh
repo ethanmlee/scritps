23905c4e69ef7884720123ea25f125c935a234c4
Ethan M. Lee <ethan@mlee.xyz>
2022-12-27 12:40:41 -0600

    set default terminal

--- smartspwn.sh
+++ smartspwn.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 wn=$(xdotool getactivewindow getwindowname)
+TERMINAL=urxvtcd
 case $wn in
-  "ssh"*) urxvtcd -title "$wn" -e $wn ;;
-  "urxvt"*) urxvtcd -cd $(echo $wn | cut -d' ' -f2- | sed "s|^~|$HOME|" ) ;;
-  *"VIM") urxvtcd -cd $(echo $wn | sed -E -n 's/.*\((.*)\).*$/\1/p' | sed "s|^~|$HOME|" ) ;;
-  *"LibreWolf") urxvtcd -cd $HOME/dwn ;;
-  "spacefm"*) urxvtcd -cd $(echo $wn | cut -d' ' -f2-) ;;
+  "ssh"*) $TERMINAL -title "$wn" -e $wn ;;
+  "zsh"*) $TERMINAL -cd $(echo $wn | cut -d' ' -f2- | sed "s|^~|$HOME|" ) ;;
+  *"VIM") $TERMINAL -cd $(echo $wn | sed -E -n 's/.*\((.*)\).*$/\1/p' | sed "s|^~|$HOME|" ) ;;
+  *"LibreWolf") $TERMINAL -cd $HOME/dwn ;;
+  "spacefm"*) $TERMINAL -cd $(echo $wn | cut -d' ' -f2-) ;;
 esac
 
 
