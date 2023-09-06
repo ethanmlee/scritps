0d6a97b6f346b2f9c4141edd39dbb921e43a7100
Ethan M. Lee <ethan@mlee.xyz>
2023-09-06 00:30:00 -0400

    fuck fuzzyfind all my friends use quicknotes

--- notes.sh
+++ notes.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-cd ~/mdn
-FILE=$(/home/ethan/git/fzf/bin/fzf --preview 'bat --style=numbers --color=always --line-range :500 {}' --preview-window=right:70%:wrap)
-[ $? = 0 ] && vim $FILE
+QUICKNOTE=~/mdn/quicknote.md
+vim $QUICKNOTE
