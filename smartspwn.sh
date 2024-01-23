da8abe7eb4cb5785b8124a39f36943a6460f7551
Ethan M. Lee <ethan@mlee.xyz>
2024-01-23 14:56:46 -0500

    new script and update smartspawn.sh

--- smartspwn.sh
+++ smartspwn.sh
@@ -2,12 +2,18 @@
 wn=$(xdotool getactivewindow getwindowname)
 TERMINAL=urxvtcd
 case $wn in
-  "("*")"*) $TERMINAL -e ssh -t $(echo $wn | awk -F"[()]" '{print $2}') "cd $(echo $wn | awk '{print $3}') ; zsh" ;; 
+
+  "("*")"*) $TERMINAL -e ssh -t $(echo $wn | awk -F"[()]" '{print $2}') "cd $(echo $wn | awk '{print $3}') ; zsh" ;;
+
   "ssh"*) $TERMINAL -title "$wn" -e $wn ;;
-  "zsh"*) $TERMINAL -cd $(echo $wn | cut -d' ' -f2- | sed "s|^~|$HOME|" ) ;;
-  *"VIM") $TERMINAL -cd $(echo $wn | sed -E -n 's/.*\((.*)\).*$/\1/p' | sed "s|^~|$HOME|" ) ;;
+
+  "zsh"*) $TERMINAL -cd $(echo $wn | cut -d' ' -f2- | sed "s|^~|$HOME|") ;;
+
+  *"NVIM") $TERMINAL -cd $(echo $wn | cut -d'(' -f2- | rev | cut -d')' -f2- | rev | sed "s|^~|$HOME|") ;;
+
+  *"VIM") $TERMINAL -cd $(echo $wn | cut -d'(' -f2- | rev | cut -d')' -f2- | rev | sed "s|^~|$HOME|") ;;
+
   *"Mozilla Firefox") $TERMINAL -cd $HOME/dwn ;;
+
   #"spacefm"*) $TERMINAL -cd $(echo $wn | cut -d' ' -f2-) ;;
 esac
-
-
