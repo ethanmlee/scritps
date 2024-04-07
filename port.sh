9e180ee9dc57f8dfcf8e3227c0e02f09cf153576
Ethan M. Lee <ethan@mlee.xyz>
2024-04-06 22:20:02 -0400

    simplify shuf command

--- port.sh
+++ port.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-  random_port=$(shuf -i 10000-49152 | head -n 1)
+  random_port=$(shuf -i 10000-49152 -n 1)
   nc -z 127.0.0.1 $random_port < /dev/null &>/dev/null
   [ $? = 0 ] && echo "$random_port" && exit
 done
